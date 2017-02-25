e4025f5a421710f45a2d3188be0cf59709f43217
Wei Lu <wlu@linkedin.com>
2017-02-24 16:13:35 -0800

    minor

--- java/ca/ubc/maxcover/Main.java
+++ java/ca/ubc/maxcover/Main.java
@@ -6,11 +6,10 @@ import java.util.logging.Logger;
  * Main class for executing jobs
  */
 public class Main {
-
     private static final Logger LOGGER = Logger.getLogger("MaxCover");
 
     public static void main(String[] args) {
         System.out.println("Hello, I am max-k-cover");
+        LOGGER.info("Hello, I am max-k-cover");
     }
-
 }
